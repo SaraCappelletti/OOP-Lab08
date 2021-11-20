b149dc219a1ec5cb05b35e1c439aad5407cd4355
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2021-11-20 16:25:31 +0100

    Started ex 4

--- src/it/unibo/oop/lab/mvc/Controller.java
+++ src/it/unibo/oop/lab/mvc/Controller.java
@@ -14,15 +14,17 @@ public interface Controller {
      * 
      * 1) A method for setting the next string to print. Null values are not
      * acceptable, and an exception should be produced
-     * 
+     */
+    /* 
      * 2) A method for getting the next string to print
-     * 
+     */
+    /* 
      * 3) A method for getting the history of the printed strings (in form of a List
      * of Strings)
-     * 
+     */
+    /* 
      * 4) A method that prints the current string. If the current string is unset,
      * an IllegalStateException should be thrown
-     * 
      */
 
 }
